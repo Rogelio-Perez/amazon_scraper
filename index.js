4fc084d5bb1bf39810b0434f368bbc48324a2d12
roger <perezmenarogelio@gmail.com>
2022-02-22 12:22:14 -0600

    Adding Endpoints

--- index.js
+++ index.js
@@ -27,4 +27,46 @@ app.get("/products/:productId", async (req, res) => {
   }
 })
 
+// GET Product Reviews
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`
+    )
+    res.json(JSON.parse(response))
+  } catch (error) {
+    res.json(error)
+  }
+})
+
+// GET Product Offers
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    )
+    res.json(JSON.parse(response))
+  } catch (error) {
+    res.json(error)
+  }
+})
+
+// GET Search Result
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`
+    )
+    res.json(JSON.parse(response))
+  } catch (error) {
+    res.json(error)
+  }
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
